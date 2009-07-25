146e769039268269ccb5d76a1f6ced5d8196d90e
przemyslaw.sitek <przemyslaw.sitek@25e2be5a-793a-11de-8775-379e36fe61d2>
2009-07-25 16:54:00 +0000

    Initial import

---
+++ cli/pwmonkey.h
@@ -0,0 +1,7 @@
+#include <glib.h>
+
+gchar *
+password_monkey_generate_pass (const gchar *domain,
+                               const gchar *password,
+                               int          len);
+
