15a2d1569de6d1de2153f97efab2bd50dd18809a
przemyslaw.sitek <przemyslaw.sitek@25e2be5a-793a-11de-8775-379e36fe61d2>
2009-07-27 16:18:00 +0000

    CTest API

--- cli/tests/ctest.c
+++ cli/tests/ctest.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdarg.h>
+
 #include "ctest.h"
 
 
@@ -21,10 +24,11 @@ ctest_free (CTest *test)
 
 
 void
-ctest_add_case (CTest *test, CTestCaseFunction func)
+ctest_add_case_full (CTest *test, const gchar *name, CTestCaseFunction func)
 {
 	CTestCase *tc = ctest_case_new (func);
 	tc->test = test;
+	tc->name = g_strdup (name);
 
 	test->test_cases = g_list_append (test->test_cases, tc);
 }
@@ -74,4 +78,54 @@ ctest_case_free (CTestCase *tc)
 {
 	g_free (tc);
 }
+
+
+/*** ASSERTIONS ***/
+
+
+void ctest_case_assert              (CTestCase *tc, gint condition, const gchar *file, gint line, const gchar *msg, ...)
+{
+	tc->asserts_total++;
+	if (condition) {
+		tc->asserts_ok++;
+	} else {
+		fprintf (stderr, " *** %s:%d: ", file, line);
+
+		va_list ap;
+		va_start (ap, msg);
+		vfprintf (stderr, msg, ap);
+		va_end (ap);
+
+		fprintf (stderr, "\n");
+	}
+}
+
+/*
+void ctest_case_assert_true         (CTestCase *tc, gint condition, const gchar *file, gint line)
+{
+	ctest_case_assert (tc, condition, file, line, "Assertion failed");
+}
+*/
+
+void ctest_case_assert_equal_int    (CTestCase *tc, gint i1, gint i2, const gchar *file, gint line)
+{
+}
+
+
+void ctest_case_assert_equal_long   (CTestCase *tc, glong l1, glong l2, const gchar *file, gint line)
+{
+}
+
+
+void ctest_case_assert_equal_string (CTestCase *tc, const gchar *s1, const gchar *s2, const gchar *file, gint line)
+{
+}
+
+
+void ctest_case_assert_not_reached  (CTestCase *tc, const gchar *file, gint line)
+{
+}
+
+
+
 
