e43056b800f5bf2dd0c632ac799542dd8afa6bba
przemyslaw.sitek <przemyslaw.sitek@25e2be5a-793a-11de-8775-379e36fe61d2>
2009-07-28 20:16:29 +0000

    Tests with datasets

---
+++ cli/tests/test-dataset.c
@@ -0,0 +1,74 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "ctest.h"
+
+struct testitem {
+	int num;
+	const char *str;
+};
+
+struct testitem sprintf_data[] = {
+	{0,           "0"},
+	{1,           "1"},
+	{2,           "2"},
+	{-1,          "-1"},
+	{-2,          "-2"},
+	{2147483647,  "2147483647"},
+	{-2147483648, "-2147483648"},
+	{7, "9"}
+};
+
+
+void
+test_sprintf (CTestCase *tc, gpointer test_data)
+{
+	const struct testitem *data = ctest_case_get_data (tc);
+
+	char buffer[32];
+
+	sprintf (buffer, "%d", data->num);
+
+	ctest_assert_equal_string (tc, data->str, buffer);
+}
+
+
+void
+test_atoi (CTestCase *tc, gpointer test_data)
+{
+	const struct testitem *data = ctest_case_get_data (tc);
+
+	int i = atoi (data->str);
+
+	ctest_assert_equal (tc, data->num, i);
+}
+
+
+void
+test_strtol (CTestCase *tc, gpointer test_data)
+{
+	const struct testitem *data = ctest_case_get_data (tc);
+
+	char *end;
+	long int i = strtol (data->str, &end, 10);
+
+	ctest_assert_equal (tc, data->num, i);
+	ctest_assert_equal_string (tc, "", end);
+}
+
+
+
+int main (int argc, char *argv[])
+{
+	CTest *test = ctest_new (NULL);
+
+	ctest_add_case_dataset (test, test_sprintf, sprintf_data);
+	ctest_add_case_dataset (test, test_atoi, sprintf_data);
+	ctest_add_case_dataset (test, test_strtol, sprintf_data);
+
+	ctest_run (test);
+
+	return ctest_finish (test);
+}
+
