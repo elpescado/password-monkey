36b65325b33de8508b1c135a9bdc62d00b5b0b94
przemyslaw.sitek <przemyslaw.sitek@25e2be5a-793a-11de-8775-379e36fe61d2>
2010-04-03 19:58:19 +0000

    Firefox extension

---
+++ trunk/firefox/defaults/preferences/passwordmonkey.js
@@ -0,0 +1,2 @@
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.passwordmonkey@elpescado.net.description", "chrome://passwordmonkey/locale/passwordmonkey.properties");
