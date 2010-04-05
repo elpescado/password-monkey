cab69b444a32934a1fcec44a7b6a73c577784e8e
przemyslaw.sitek <przemyslaw.sitek@25e2be5a-793a-11de-8775-379e36fe61d2>
2010-04-05 12:38:44 +0000

    Major algorithm redesign, documentation, unit tests

--- cli/pwmonkey.c
+++ cli/pwmonkey.c
@@ -12,41 +12,85 @@
 
 #define PWMON_ID "5dfabc489612601f0e253781fbc6f009"
 
-gchar *
-password_monkey_generate_pass (const gchar *domain,
-                               const gchar *password,
-                               int          len)
+static void
+_print_hex_hash (const guint8 *data, size_t len)
 {
-	/* Generate identifier */
-	gchar *data = g_strdup_printf ("pwmon(%s;%s;%s)",
-                                       domain,
-                                       password,
-                                       PWMON_ID);
+	int i;
+	for (i = 0; i < len; i++) {
+		g_print ("%02x", (int)data[i]);
+	}
+	g_print ("\n");
+}
 
-	/* Compute checksum */
+
+/**
+ * IDENT operation
+ *
+ * Return: newly allocated string to be freed with g_free()
+ **/
+gchar *
+password_monkey_ident (const gchar *domain,
+                       const gchar *password)
+{
+	return g_strdup_printf ("pwmon(%s;%s;%s)",
+                                domain,
+                                password,
+                                PWMON_ID);
+}
+
+
+/**
+ * HASH operation
+ *
+ * Return: Newly allocated buffer
+ **/
+guint8 *
+password_monkey_hash (const gchar *data)
+{
 #ifdef USE_OPENSSL
+/*
 	guint8 *digest = g_new (guint8, SHA_DIGEST_LENGTH);
 	SHA1 (data, strlen (data), digest);
 	gsize digest_len = SHA_DIGEST_LENGTH;
-#elif
-	GChecksumType hash_type = G_CHECKSUM_SHA1;
+*/
+#else
+	GChecksumType hash_type = G_CHECKSUM_SHA256;
+	gssize hash_len = g_checksum_type_get_length (hash_type);
 	guint8 *digest = g_new (guint8, hash_len);
 	GChecksum *hash = g_checksum_new (hash_type);
 	g_checksum_update (hash, (guchar*) data, strlen (data));
-	gssize hash_len = g_checksum_type_get_length (hash_type);
 	gsize digest_len = hash_len;
 	g_checksum_get_digest (hash, digest, &digest_len);
 	g_checksum_free (hash);
 #endif
 
+	return digest;
+}
 
-	g_free (data);
 
-	/* Encode as Base-64 and trim */
-	gchar *encoded = g_base64_encode (digest, digest_len);
-	gchar *trimmed = g_strndup (encoded, len);
-	g_free (encoded);
+gchar *
+password_monkey_encode (const guint8 *digest)
+{
+	return g_base64_encode (digest, 32);
+}
 
+
+gchar *password_monkey_truncate (const gchar *encoded,
+                                 guint        len)
+{
+	return g_strndup (encoded, len);
+}
+
+
+gchar *
+password_monkey_generate_pass (const gchar *domain,
+                               const gchar *password,
+                               int          len)
+{
+	gchar *id = password_monkey_ident (domain, password);
+	guint8 *hash = password_monkey_hash (id); g_free (id);
+	gchar *encoded = password_monkey_encode (hash); g_free (hash);
+	gchar *trimmed = password_monkey_truncate (encoded, len); g_free (encoded);
 	return trimmed;
 }
 
---
+++ tests/test-cli.c
@@ -0,0 +1,118 @@
+#include <string.h>
+#include <stdlib.h>
+
+#include "pwmonkey.h"
+#include "ctest.h"
+#include "csv.h"
+
+static void
+hex2bin (const gchar *hex, guint8 *bin)
+{
+	size_t len = strlen (hex);
+	gint i;
+
+	for (i = 0; i < len/2; i++) {
+		gchar tmp[2] = {0};
+		strncpy (tmp, hex+2*i, 2);
+		bin[i] = (int) strtol (tmp, NULL, 16);
+	}
+}
+
+
+void
+test_ident (CTestCase *tc, gpointer data)
+{
+	CsvFile *csv = csv_file_load ("data/ident.csv");
+
+	int i;
+	for (i = 0; i < csv_file_num_rows (csv); i++) {
+		const gchar *domain = csv_file_get_data (csv, i, 0);
+		const gchar *password = csv_file_get_data (csv, i, 1);
+		const gchar *expected_id = csv_file_get_data (csv, i, 2);
+		gchar *id = password_monkey_ident (domain, password);
+		ctest_assert_equal_string (tc, expected_id, id);
+		g_free (id);
+	}
+
+	csv_file_free (csv);
+}
+
+
+void
+test_hash (CTestCase *tc, gpointer data)
+{
+	CsvFile *csv = csv_file_load ("data/hash.csv");
+
+	int i;
+	for (i = 0; i < csv_file_num_rows (csv); i++) {
+		const gchar *string = csv_file_get_data (csv, i, 0);
+		const gchar *hex_hash = csv_file_get_data (csv, i, 1);
+		guint8 expected_hash[32];
+		hex2bin (hex_hash, expected_hash);
+
+		guint8 *hash = password_monkey_hash (string);
+		
+		ctest_assert (tc, memcmp (expected_hash, hash, 32) == 0, "Failurerow %d", i);
+		g_free (hash);
+	}
+
+	csv_file_free (csv);
+}
+
+
+void
+test_encode (CTestCase *tc, gpointer data)
+{
+	CsvFile *csv = csv_file_load ("data/encode.csv");
+
+	int i;
+	for (i = 0; i < csv_file_num_rows (csv); i++) {
+		const gchar *hex_hash = csv_file_get_data (csv, i, 0);
+		const gchar *expected_b64 = csv_file_get_data (csv, i, 1);
+		guint8 hash[32];
+		hex2bin (hex_hash, hash);
+
+		gchar *b64 = password_monkey_encode (hash);
+		ctest_assert_equal_string (tc, expected_b64, b64);
+		g_free (b64);		
+	}
+
+	csv_file_free (csv);
+}
+
+
+void
+test_truncate (CTestCase *tc, gpointer data)
+{
+	CsvFile *csv = csv_file_load ("data/truncate.csv");
+
+	int i;
+	for (i = 0; i < csv_file_num_rows (csv); i++) {
+		const gchar *subject = csv_file_get_data (csv, i, 0);
+		const gchar *len_str = csv_file_get_data (csv, i, 1);
+		const gchar *expected = csv_file_get_data (csv, i, 2);
+		gint len = atoi (len_str);
+
+		gchar *res = password_monkey_truncate (subject, len);
+		ctest_assert_equal_string (tc, expected, res);
+		g_free (res);		
+	}
+
+	csv_file_free (csv);
+}
+
+
+
+int main (int argc, char *argv[])
+{
+	CTest *test = ctest_new (NULL);
+
+	ctest_add_case (test, test_ident);
+	ctest_add_case (test, test_hash);
+	ctest_add_case (test, test_encode);
+	ctest_add_case (test, test_truncate);
+
+	ctest_run (test);
+
+	return ctest_finish (test);
+}
