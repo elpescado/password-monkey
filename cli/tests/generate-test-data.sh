bd1b5c75e9fc7ac9a2065b8358bfc0cfe31bc8fd
przemyslaw.sitek <przemyslaw.sitek@25e2be5a-793a-11de-8775-379e36fe61d2>
2010-05-04 14:43:03 +0000

    Firefox extension

--- cli/tests/generate-test-data.sh
+++ cli/tests/generate-test-data.sh
@@ -1,6 +1,6 @@
 DOMAINS="example.com example.org google.com code.google.com elpescado.net przemyslawsitek.pl"
 PASSWORDS="1234 pwd passw0rd t0p-s3cret foo sH4l1-1-c0mP4r3-Th33-t0-4-sUmm3r5-dAy"
-LENGTHS="10 12 16 20 28"
+LENGTHS="10 12 16 20 28 32 44"
 PWMONKEY=../pwmonkey
 
 for DOMAIN in $DOMAINS
@@ -10,7 +10,7 @@ do
 		for LENGTH in $LENGTHS
 		do
 			PWD=`$PWMONKEY -d "$DOMAIN" -p "$PASSWORD" -l $LENGTH`
-			echo "{\"$PASSWORD\", \"$DOMAIN\", $LENGTH, \"$PWD\"},"
+			echo "$PASSWORD,$DOMAIN,$LENGTH,$PWD"
 		done
 	done
 done
